f20aba8bb24ec9fd205f8f691c99fd5ba2b681dd
Ryan Benedetti <rhino.j.benedetti@gmail.com>
2014-08-13 15:54:59 -0700

    Routes -> Show page complete.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,6 +3,14 @@ class MoviesController < ApplicationController
     #to test for pluralize @movies = Movie.limit(1)
    @movies = Movie.all
   end
+  
+  def show
+    #to test for pluralize @movies = Movie.limit(1)
+   @movie = Movie.find(params[:id])
+  end
+  
 end
 
 # Iteration 1: @movies = ['Guardians of the Galaxy','Thor: The Dark World','The Avengers', 'X-Men: Days of Future Passed']
+#Iteration 2: Connect to database through Model
+#Iteration 3: Add show action to dispay a detail page. Pass id parameters.
